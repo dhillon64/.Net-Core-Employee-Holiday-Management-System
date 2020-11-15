81101dc26e359cc50c42e48e531f4856e540d004
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-11-15 16:02:39 +0000

    Add project files.

---
+++ EmployeeHolidayManagement/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeHolidayManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveHistory> LeaveHistories { get; set; }
+    }
+}
---
+++ EmployeeHolidayManagement/Data/Employee.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeHolidayManagement.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string TaxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+        
+    }
+}
